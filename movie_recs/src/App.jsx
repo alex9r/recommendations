d0e0781a97eb6f8a52cbf1d11766c6f5709d400a
alex9r <89540509+alex9r@users.noreply.github.com>
2025-12-08 10:14:50 -0500

    adding basic card logic

--- movie_recs/src/App.jsx
+++ movie_recs/src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const Card = ({title} ) => {
+
+  const [liked, setLiked] = useState(false);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${liked}`);
+  }, [liked]);
+
+  useEffect(() => {
+    console.log(`CARD RENDERED`);
+  }, []);
+  
+  return (
+    <div className='card' onClick={() => setCount(count + 1)}>
+      <h2>{title} <br/> {count} </h2>
+      <button onClick={() => setLiked(!liked)}>
+        {liked ? '❤️' : '🩶'}
+      </button>
+    </div>
+  )
+}
+const App = () => {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='card-container'>
+        <Card title="Avatar" rating={5} actors={[{name : 'actors'}]} />
+        <Card title="Me Before You" />
+        <Card title="Harry Potter and the Philosopher's Stone" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
