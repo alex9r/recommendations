96f169a215a69f31d473200ae31cd8333074912d
alex9r <89540509+alex9r@users.noreply.github.com>
2025-12-08 14:48:58 -0500

    appwrite db logic to app.jsx

--- movie_recs/src/App.jsx
+++ movie_recs/src/App.jsx
@@ -52,6 +52,10 @@ const App = () => {
 
       setMovieList(data.results || []);
 
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
+
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage(`Error fetching movies. Please try again.`);
