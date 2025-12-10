1578bd56f2c7a838c1e3980c9c5069a2b2a2c26d
alex9r <89540509+alex9r@users.noreply.github.com>
2025-12-09 20:50:29 -0500

    adding link to movieCard component

--- movie_recs/src/components/movieCard.jsx
+++ movie_recs/src/components/movieCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const MovieCard = ({movie: {id, title, vote_average, poster_path, release_date, original_language}}) => {
+const MovieCard = ({movie: {id, title, vote_average, poster_path, release_date, original_language, imbd_id}}) => {
   return (
     <div className="movie-card">
         <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : `/no-movie.svg`} 
@@ -8,6 +8,12 @@ const MovieCard = ({movie: {id, title, vote_average, poster_path, release_date, 
         />
         < div className="mt-4">
             <h3>{title}</h3>
+            <a href={`https://www.imdb.com/title/${imbd_id}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            title="View on IMDb">
+                🔗
+            </a>
             <div className="content">
                 <div className="rating">
                     <p>⭐️</p>
